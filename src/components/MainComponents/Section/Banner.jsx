a5619a615c7c5bd9668880817bc7593f9d0217f1
Oscar Poveda <oscarpoveda679@gmail.com>
2024-01-04 09:02:27 -0500

    mejoras en el banner

--- src/components/MainComponents/Section/Banner.jsx
+++ src/components/MainComponents/Section/Banner.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 
 
-const Banner = ({ color ,title,children, img}) => {
-    // const bgColor = color ?? '#E9A763';
+const Banner = ({ numero ,title,children, img}) => {
+    const bgColor1 = '#E9A763';
+    const bgColor2 = '#343A3F';
     return (
         <>
-            <div className={`bg-[#E9A763] text-white flex justify-between rounded-3xl font-medium  md:w-full w-full md:ml-2 `}>
+            <div className={`bg-[${ numero === 1 ? bgColor1 : bgColor2}] text-white flex justify-between rounded-3xl font-medium  md:w-full w-full md:ml-2 `}>
                 <div className="px-14 pt-5  text-white-50 w-2/3 flex flex-col justify-between">
                     <h1 className="text-2xl leading-tight font-normal">
                     {title} 
@@ -29,7 +30,7 @@ const Banner = ({ color ,title,children, img}) => {
 Banner.propTypes = {
     title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
-    color: PropTypes.string,
+    numero: PropTypes.number,
     img: PropTypes.string.isRequired
 }
 export default Banner;
--- src/components/MainComponents/Section/Section.jsx
+++ src/components/MainComponents/Section/Section.jsx
@@ -18,7 +18,7 @@ const Section = () => {
       <Banner
         title={"Hola, Andres! Tienes idea sobre un nuevo libro?"}
         img={Books}
-        color={"#E9A763"}
+        numero={1}
       >
         <div className="flex flex-col gap-2">
           <div className="text-md">
@@ -46,7 +46,7 @@ const Section = () => {
       <div className="mb-4 ">
         <Banner
           img={Niños}
-          color={"#343A3F"}
+          numero={2}
           title={
             "Andrés, nos gusta tu contenido y queremos darte los siguientes tips:"
           }
